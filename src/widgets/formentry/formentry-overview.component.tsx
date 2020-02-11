12c29643af02b457772b0184976cf13c2ec23662
ojwanganto <ojwangwachiaje@gmail.com>
2020-02-11 15:35:02 +0300

    add ability to open form on new tab

--- src/widgets/formentry/formentry-overview.component.tsx
+++ src/widgets/formentry/formentry-overview.component.tsx
@@ -10,6 +10,7 @@ import HorizontalLabelValue from "../cards/horizontal-label-value.component";
 import { useCurrentPatient } from "@openmrs/esm-api";
 import SummaryCardFooter from "../cards/summary-card-footer.component";
 import { Trans, useTranslation } from "react-i18next";
+import styles from "./formentry-overview.css";
 
 export default function PatientFormsOverview(props: PatientFormProps) {
   const [patientForms, setPatientForms] = React.useState([]);
@@ -23,28 +24,19 @@ export default function PatientFormsOverview(props: PatientFormProps) {
 
   React.useEffect(() => {
     if (patientUuid) {
-      const forms = [];
-      forms.push({
-        uuid: "12345",
-        html: "<html>",
-        formName: "Test Form1",
-        formDesc: "Testing forms component"
-      });
-      forms.push({
-        uuid: "45678",
-        html: "<html>",
-        formName: "Test Form2",
-        formDesc: "Testing forms component"
-      });
       const subscription = fetchPatientForms(patientUuid).subscribe(
         pForms => setPatientForms(pForms),
         createErrorHandler()
       );
 
-      console.log(patientForms);
       return () => subscription.unsubscribe();
     }
   }, [patientUuid]);
+
+  let handleOnClick = (event, linktToOpen) => {
+    event.preventDefault();
+    window.open(linktToOpen);
+  };
 
   return (
     <SummaryCard
@@ -71,18 +63,18 @@ export default function PatientFormsOverview(props: PatientFormProps) {
       </SummaryCardRow>
       {patientForms &&
         patientForms.map(f => {
-          let fResource = `${
-            new URL(window.location.href).hostname
-          }/htmlformentryui/htmlform/enterHtmlFormWithStandardUi.page?patientId=${patientUuid}&visitId=839c12f2-163e-4ccd-ba6f-97446898c60f&definitionUiResource=referenceapplication:htmlforms/simpleVisitNote.xml&returnUrl=/openmrs/coreapps/patientdashboard/patientDashboard.page/patientId=eb359f72-2b2c-4a08-a5b5-cac9361dc705%26c12f2-163e-4ccd-ba6f-97446898c60f&visitId=2385`;
+          let fResource = `/openmrs/htmlformentryui/htmlform/enterHtmlFormWithStandardUi.page?patientId=${patientUuid}&visitId=839c12f2-163e-4ccd-ba6f-97446898c60f&definitionUiResource=referenceapplication:htmlforms/simpleVisitNote.xml&returnUrl=/openmrs/coreapps/patientdashboard/patientDashboard.page/patientId=eb359f72-2b2c-4a08-a5b5-cac9361dc705%26c12f2-163e-4ccd-ba6f-97446898c60f&visitId=2385`;
           return (
-            <SummaryCardRow key={f.uuid} linkTo={fResource}>
-              <HorizontalLabelValue
-                label={f.display}
-                labelStyles={{ fontWeight: 500 }}
-                value={f.formName}
-                valueStyles={{ fontFamily: "Work Sans" }}
-              />
-            </SummaryCardRow>
+            <div className={styles.formElement} key={f.uuid}>
+              <a
+                className={styles.formLink}
+                onClick={event => {
+                  handleOnClick(event, fResource);
+                }}
+              >
+                {f.display}
+              </a>
+            </div>
           );
         })}
       <SummaryCardFooter linkTo={`/patient/${patientUuid}/chart/forms`} />
